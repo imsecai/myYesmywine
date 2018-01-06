e7b25038713406a601ed3091137d378889d92004
imsecai <wei695153881@126.com>
2018-01-06 18:19:01 +0800

    数据

--- src/js/reg.js
+++ src/js/reg.js
@@ -1,25 +1,73 @@
 require(['config'],function(){
 	require(['jquery'],function($){
+		function vcode(){
+		    var res="";
+		    for(var i=0;i<4;i++){
+		        res+=parseInt(Math.random()*10);
+		    }
+		    return res;
+		}
+		var yzcode=document.getElementById("vcode");
+		var yzm=vcode();
+		yzcode.innerHTML=yzm;
 		$('.btnReg').on('click',function(){
 			var _username = $('#username').val();
 			var _password = $('#password').val();
+			var _password2 = $('#password2').val();
+			var _phone = $('#phone').val();
+			var _code = $('#code').val();
 
+			// var phone = document.getElementById('phone').value;
+			var reg = /^1[34578]\d{9}$/i
+			if(_phone === ''){
+				alert('手机号不能为空');
+				return false;
+			}
+			else if(!reg.test(_phone)){
+				alert('手机号不合法');
+				return false;
+			}
+
+			if(_code === ''){
+				alert('请输入验证码');
+				return false;
+			}
+			else if(_code != yzm){
+				alert('验证码不正确');
+				return false;
+			}
+
+			if(_password === ''){
+				alert('密码不能为空');
+				return false;
+			}
+
+			if(_password != _password2){
+				alert('两次密码不一致');
+				return false;
+			}
 			// 发起ajax请求
 			$.ajax({
 				// hardcode
-				url:'../mysql/reg.php',
+				url:'../php/reg.php',
 				data:{
 					username:_username,
+					phone:_phone,
 					password:_password
 				},
 				success:function(data){
 					if(data === 'fail'){
-
+						alert('用户已存在，请登录');
 						return;
+					}else if(data === 'ok'){
+					console.log(data)
+
+					}else if(data === 'error'){
+						alert('密码或账号不正确');
+						return false;
 					}
 
 					
-					console.log(data)
 				}
 			})
 		})
--- src/php/reg.php
+++ src/php/reg.php
@@ -3,14 +3,10 @@
 	
 	$username = isset($_GET['username']) ? $_GET['username'] : '';
 	$password = isset($_GET['password']) ? $_GET['password'] : '123456';
-	// $email = isset($_GET['email']) ? $_GET['email'] : '';
-	// $grade = isset($_GET['grade']) ? $_GET['grade'] : '';
-	// $gender = isset($_GET['gender']) ? $_GET['gender'] : '';
-	// $birthday = isset($_GET['birthday']) ? $_GET['birthday'] : '';
-	// $phone = isset($_GET['phone']) ? $_GET['phone'] : '';
+	$phone = isset($_GET['phone']) ? $_GET['phone'] : '';
 
 	//查看用户名是否已经存在
-	$sql = "select username from user where username='$username'";
+	$sql = "select phone from user where phone='$phone'";
 	$result = $conn->query($sql);
 	if($result->num_rows>0){
 		echo "fail";
@@ -26,7 +22,7 @@
 		 */
 		// $password = password_hash($password,PASSWORD_DEFAULT);
 
-		$sql = "insert into user (username,password) values('$username','$password')";
+		$sql = "insert into user (phone,password) values('$phone','$password')";
 
 
 		// 获取查询结果
@@ -35,12 +31,9 @@
 		if ($result) {
 		    echo "ok";
 		} else {
-		    echo "Error: " . $sql . "<br>" . $conn->error;
+		    echo "error: " . $sql . "<br>" . $conn->error;
 		}
 	}
-
-	
-	
 
 	// 释放查询内存(销毁)
 	//$result->free();
